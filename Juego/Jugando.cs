7968e9ebfa82421b2e62d77cb63f765d8aa5e623
Zeta <zetag174@gmail.com>
2024-05-07 11:22:16 -0600

    Comodines

--- Juego/Jugando.cs
+++ Juego/Jugando.cs
@@ -11,14 +11,17 @@ namespace Juego
             
             Random random = new Random();
 
-            for(int x = 10; x <= state.Turnos; x--)
+            for(int x = 10; x >= state.Turnos; x--)
             {
-                 bool comodines = true;
-
-
+                bool comodines = true;
                 int numerodado = random.Next(1, 6);
                 int numerodado2 =random.Next(1, 6);
                 int numerodado3 =random.Next(1, 6);
+
+
+                Pantalla:
+
+                Console.Clear();
                 Console.WriteLine($"Vidas: {state.Vidas}  Turno: {x} Comodines: {state.Comodines} Puntos: {state.Puntos} \n ");
                 Console.WriteLine($"Numeros requeridos: {numerodado} {numerodado2} {numerodado3}\n");
                 Console.WriteLine(@$"
@@ -27,23 +30,39 @@ namespace Juego
                 Ingrese 3 para guardar y salir");
                 int opcion = int.Parse(Console.ReadLine());
 
+                
                 switch (opcion) {
                     case 1:
+
                         int dado1 = random.Next(1, 6);
                         int dado2 = random.Next(1, 6);
-                        int dado3 = random.Next(1, 6);
-                        Console.WriteLine($"Tirando dados... {dado1} {dado2} {dado3}");
-                        if (dado1 == numerodado || dado2 == numerodado || dado3 == numerodado)
+                       
+                        Console.WriteLine($"Tirando dados... {dado1} {dado2}");
+                        if (dado1 == numerodado || dado2 == numerodado )
                         {
                             Console.WriteLine("Has acertado el primer número!");
                             state.Puntos++;
-                            return;
+                           
                         }
+                        else if (dado1 == numerodado2 || dado2 == numerodado2 ){
+                            Console.WriteLine("Has acertado el segundo número!");
+                            state.Puntos++;
+                            
+                        }
+
+                        else if (dado1 == numerodado3 || dado2 == numerodado3 )
+                        {
+                            Console.WriteLine("Has acertado el tercer número!");
+                            state.Puntos++;
+                            
+                        }
+                        
                         else
                         {
                             Console.WriteLine("No has acertado los números requeridos.");
                             state.Vidas--;
                         }
+                        Console.ReadKey();
                         break;
                     case 2:
                             if (comodines == true){
@@ -53,23 +72,57 @@ namespace Juego
                                     int dado = random.Next(1, 6);
                                     Console.WriteLine("Usando comodín...");
 
+                                //* Switch Comodines
                                     switch (dado){
-                                        
+                                        case 1:
+                                            Console.WriteLine($"Numero {dado} 1 punto extra");
+                                            state.Puntos++;
+                                            break;
+                                        case 2:
+                                            Console.WriteLine($"Numero {dado} 1 turno extra");
+                                            x++;
+                                            break;
+                                        case 3:
+                                         Console.WriteLine($"Numero {dado} 2 puntos extra");
+                                            state.Puntos++;
+                                            state.Puntos++;
+                                            break;
+                                        case 4:
+                                            Console.WriteLine($"Numero {dado} 1 punto menos");
+                                            state.Puntos--;
+                                            break;
+                                        case 5:
+                                            Console.WriteLine($"Numero {dado} 1 turno menos");
+                                            x--;
+                                            break;
+                                        case 6:Console.WriteLine($"Numero {dado} perdiste este comodin");
+                                            break;
+
                                     }
 
+                                    Console.ReadKey();
                                     state.Comodines--;
                                     comodines=false;
+                                    goto Pantalla;
 
                                 }
                                 else
                                 {
                                     Console.WriteLine("No tienes comodines disponibles.");
+                                    Console.ReadKey();
+                                    goto Pantalla;
                                 }
                             }
                             else{
                                 Console.WriteLine("Ya has usado un comodín.");
+                                Console.ReadKey();
+                                    goto Pantalla;
                             }
-                        break;
+                    case 3:
+                    Console.WriteLine("Guardando partida...");
+                    Datos.Game.SaveGame(state);
+                    Console.ReadKey();
+                    return;
                 }
             }
 
--- obj/Debug/net8.0/CartaKill.AssemblyInfo.cs
+++ obj/Debug/net8.0/CartaKill.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CartaKill")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6c24e6182005a2e3dff1d6b4bca074e26e86aeff")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4722a42d67ea26553cc4e54a173af198bc712f06")]
 [assembly: System.Reflection.AssemblyProductAttribute("CartaKill")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CartaKill")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

4624483589dee565d4290667542f0dacee468a68
Zeta <zetag174@gmail.com>
2024-05-06 22:04:15 -0600

    Inicio

---
+++ Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using Fonts;
+using Juego;
+
+namespace KillerCard
+{
+    class Program
+    {
+           
+
+        
+        static void Main(string[] args)
+        {
+            
+
+            Fonts.ConsoleUtils.MasFontSize();
+            
+            
+
+            Seleccion:
+            Console.Write("    ");
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.ForegroundColor = ConsoleColor.White;
+
+            Console.Write("Killer Card");
+            
+
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ForegroundColor = ConsoleColor.White;
+
+            Console.WriteLine(@$"
+            
+    1.Jugar
+    2.Cargar Partida
+    3.Creditos
+    4.Salir");
+
+           
+            int opcion = 0;
+            
+                  opcion = int.Parse(Console.ReadLine());
+            
+            
+            
+           
+
+            switch (opcion)
+                        {
+                            
+                            case 1:
+                                Console.Clear();
+                                Juego.Juego.Jugar();
+                                break;
+                            case 2:
+                                Console.Clear();
+                                Juego.Juego.CargarPartida();
+                                break;
+                            case 3:
+                                Console.Clear();
+                                Juego.Juego.Creditos();
+                                break;
+                            case 4:
+                                Console.Clear();
+                                Juego.Juego.Salir();
+                                break;
+                            default:
+                                
+                                Console.WriteLine("Opcion no valida");
+                                Console.ReadKey();
+                                Console.Clear();
+                                
+
+                                goto Seleccion;
+                        }
+        }
+    }
+}
---
+++ funciones.cs
@@ -0,0 +1,65 @@
+using System;
+using System.Runtime.InteropServices;
+
+namespace Fonts{
+    public static class ConsoleUtils
+{
+    private const int STD_OUTPUT_HANDLE = -11;
+    private const int TMPF_TRUETYPE = 4;
+
+    [DllImport("kernel32.dll")]
+    private static extern IntPtr GetStdHandle(int handle);
+
+    [DllImport("kernel32.dll")]
+    private static extern bool GetCurrentConsoleFontEx(IntPtr consoleOutput, bool maximumWindow, ref CONSOLE_FONT_INFO_EX consoleFontInfo);
+
+    [DllImport("kernel32.dll")]
+    private static extern bool SetCurrentConsoleFontEx(IntPtr consoleOutput, bool maximumWindow, ref CONSOLE_FONT_INFO_EX consoleFontInfo);
+
+    [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Unicode)]
+    private struct CONSOLE_FONT_INFO_EX
+    {
+        public int cbSize;
+        public int nFont;
+        public COORD dwFontSize;
+        public int FontFamily;
+        public int FontWeight;
+        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 32)]
+        public string FaceName;
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    private struct COORD
+    {
+        public short X;
+        public short Y;
+    }
+
+    public static void MasFontSize()
+    {
+        IntPtr consoleOutput = GetStdHandle(STD_OUTPUT_HANDLE);
+        CONSOLE_FONT_INFO_EX consoleFontInfo = new CONSOLE_FONT_INFO_EX();
+        consoleFontInfo.cbSize = Marshal.SizeOf(consoleFontInfo);
+
+        if (GetCurrentConsoleFontEx(consoleOutput, false, ref consoleFontInfo))
+        {
+            consoleFontInfo.dwFontSize.Y += 20; // Increase the font size by 2
+            SetCurrentConsoleFontEx(consoleOutput, false, ref consoleFontInfo);
+        }
+    }
+
+    public static void ResetFontSize()
+    {
+        IntPtr consoleOutput = GetStdHandle(STD_OUTPUT_HANDLE);
+        CONSOLE_FONT_INFO_EX consoleFontInfo = new CONSOLE_FONT_INFO_EX();
+        consoleFontInfo.cbSize = Marshal.SizeOf(consoleFontInfo);
+
+        if (GetCurrentConsoleFontEx(consoleOutput, false, ref consoleFontInfo))
+        {
+            consoleFontInfo.dwFontSize.Y -= 2; // Decrease the font size by 2 to restore it to the original size
+            SetCurrentConsoleFontEx(consoleOutput, false, ref consoleFontInfo);
+        }
+    }
+}
+
+}
